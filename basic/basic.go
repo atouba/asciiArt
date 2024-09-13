a362c14641efc5a679f538d9821985b4154d1b02
atouba <anasstouba5@gmail.com>
2024-09-13 17:57:16 +0300

    refactor and now can print mulitple new lines

--- basic/basic.go
+++ basic/basic.go
@@ -6,29 +6,38 @@ import (
 	"github.com/atouba/piscine"
 )
 
-func Basic(str, banner string) {
-	chars, err := os.ReadFile("./banners/" + banner + ".txt")
+func printBasic(str, banner string) {
+	asciiArtChars, err := os.ReadFile("./banners/" + banner + ".txt")
 	if err != nil {
 		fmt.Println("Error reading banner file")
 		return
 	}
+	lines := piscine.Split(string(asciiArtChars), "\n")
+  for iLine := range 8 {
+    for _, char := range str {
+      if char == ' ' {
+        fmt.Print(lines[(int(char) - 32) * 8 + iLine][0:4])
+      } else {
+        fmt.Print(lines[(int(char) - 32) * 8 + iLine])
+      }
+    }
+    fmt.Println()
+  }
+}
 
-	inline_strings := piscine.Split(str, "\\n")
-	lines := piscine.Split(string(chars), "\n")
-	for i_str, inline_str := range inline_strings {
-		for i_octal_line := range 8 {
-			for _, char := range inline_str {
-				if char == ' ' {
-					fmt.Print(lines[(int(char) - 32) * 8 + i_octal_line][0:4])
-				} else {
-					fmt.Print(lines[(int(char) - 32) * 8 + i_octal_line])
-				}
-			}
-			fmt.Println()
-		}
-		if i_str + 1 < len(inline_strings) && inline_strings[i_str + 1] == "" {
-			fmt.Println()
-		}
+func Basic(str, banner string) {
+
+  for i := 0; i < len(str); {
+    endStringIndex := piscine.Index(str[i: ], "\\n")
+    if endStringIndex == -1 {
+      endStringIndex = len(str)
+    }
+    printBasic(str[i: endStringIndex], banner)
+    // adding the length of the printed string
+    i = endStringIndex
+    for ; i + 1 < len(str) && str[i: i + 2] == "\\n"; i += 2 {
+      fmt.Println()
+    }
 	}
 }
 
