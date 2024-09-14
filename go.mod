b8cbfaecb3f9d1188c906b52e9d2918684c6c0dd
atouba <anasstouba5@gmail.com>
2024-09-14 13:39:08 +0300

    structure code; import gitea hosted packages instead;

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
-module github.com/atouba/ascii_art
+module 01.gritlab.ax/git/atouba/ascii-art
 
-go 1.23.0
-
-toolchain go1.23.1
+go 1.23.1
 
 require github.com/atouba/piscine v0.0.0-20240912123319-f8b51fa00b6c
 
--- main.go
+++ main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"github.com/atouba/ascii_art/basic"
+
+	"01.gritlab.ax/git/atouba/ascii-art/basic"
+	"01.gritlab.ax/git/atouba/ascii-art/color"
 )
 
 func main() {
-	if len(os.Args) == 1 || len(os.Args) >= 4 {
+	if len(os.Args) == 1 || len(os.Args) > 4 {
 		fmt.Println(`Error: usage:
 		./ascii_art <string>
 		or
 		./ascii_art <string> <banner>`)
 		return
 	}
-	if len(os.Args) == 2 || os.Args[2] == "standard" {
+	if len(os.Args) == 2 {
 		basic.Basic(os.Args[1], "standard")
-	} else {
+	} else if len(os.Args) == 3 {
 		basic.Basic(os.Args[1], os.Args[2])
-	}
+	} else if len(os.Args) == 4 {
+    specifiedColor := os.Args[1][8:]
+    _, ok := color.Colors[specifiedColor]
+    if !ok {
+      log.Fatal("error: color doesn't exist")
+    }
+    color.Color(os.Args[3], os.Args[2], specifiedColor, "standard")
+  }
 }
