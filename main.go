7b24b8028854ab5b74bd7f7e2094fa5614bc6c82
atouba <anasstouba5@gmail.com>
2024-09-14 14:10:49 +0300

    refactor; handle different kinds of arguments

--- main.go
+++ main.go
@@ -1,32 +1,38 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
 
 	"01.gritlab.ax/git/atouba/ascii-art/basic"
 	"01.gritlab.ax/git/atouba/ascii-art/color"
+	"github.com/atouba/piscine"
 )
 
 func main() {
-	if len(os.Args) == 1 || len(os.Args) > 4 {
-		fmt.Println(`Error: usage:
-		./ascii_art <string>
-		or
-		./ascii_art <string> <banner>`)
-		return
+  args := os.Args[1: ]
+  argsLength := len(args)
+
+	if argsLength == 0 || argsLength > 3 {
+    log.Fatal(`Usage:
+      ./ascii_art <string>
+      or
+      ./ascii_art <string> <banner>
+      or
+      ./ascii_art . [OPTION] [STRING]`)
 	}
-	if len(os.Args) == 2 {
-		basic.Basic(os.Args[1], "standard")
-	} else if len(os.Args) == 3 {
-		basic.Basic(os.Args[1], os.Args[2])
-	} else if len(os.Args) == 4 {
-    specifiedColor := os.Args[1][8:]
+	if argsLength == 1 {
+		basic.Basic(args[0], "standard")
+	} else if argsLength == 2 && piscine.Index(args[0], "--color=") == -1  {
+		basic.Basic(args[0], args[1])
+	} else {
+    specifiedColor := args[0][8:]
+    if argsLength == 2 { args = append(args, args[1]) }
     _, ok := color.Colors[specifiedColor]
     if !ok {
       log.Fatal("error: color doesn't exist")
     }
-    color.Color(os.Args[3], os.Args[2], specifiedColor, "standard")
+    color.Color(args[2], args[1], specifiedColor, "standard")
   }
 }
