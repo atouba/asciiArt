8ca14427a2093622a6db13bed8100499707d33cd
atouba <anasstouba5@gmail.com>
2024-09-17 17:39:03 +0300

    fix reverse (just inline strings)

--- reverse/reverse.go
+++ reverse/reverse.go
@@ -2,7 +2,9 @@ package reverse
 
 import (
 	"fmt"
+	"log"
 	"os"
+
 	"github.com/atouba/piscine"
 )
 
@@ -41,6 +43,14 @@ func isSpace(lines *[]string, column_index int) bool {
 func asciiArtCharLength(lines *[]string, start_index int) int {
 	length := 0
 
+  // check first if it's an ascii art space
+  x := start_index
+  for ; x < start_index + 6 ; x++ {
+    if !(x < len((*lines)[0])) { log.Fatal("Bad Input") }
+    if !isSpace(lines, x) { break }
+  }
+  if x == start_index + 6 { return 6 }
+
 	for i := start_index; i < len((*lines)[0]); i++ {
 		if !isSpace(lines, i) {
 			length++
@@ -48,26 +58,25 @@ func asciiArtCharLength(lines *[]string, start_index int) int {
 			break
 		}
 	}
-	return length
+	return length + 1
 }
 
 // always assuming inline strings
 func Reverse(filename string) {
-	if len(filename) <= 10 || piscine.Compare(filename[0:10], "--reverse=") != 0 {
-		fmt.Println(`Usage: go run . [OPTION]
+// 	if len(filename) <= 10 || piscine.Compare(filename[0:10], "--reverse=") != 0 {
+// 		fmt.Println(`Usage: go run . [OPTION]
 
-EX: go run . --reverse=<fileName>`)
-		return
-	}
-	ascii_art, err := os.ReadFile(filename[10:])
+// EX: go run . --reverse=<fileName>`)
+// 		return
+// 	}
+	ascii_art, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Println("error reading file")
 		return
 	}
 	lines := piscine.Split(string(ascii_art), "\n")
-	
-	// column_i < len(lines[0]) - 1 becasue the line has an extra char '\n'
-	for column_i := 0; column_i < len(lines[0]) - 1; column_i += asciiArtCharLength(&lines, column_i) + 1 {
+  lines = lines[: len(lines)-1]
+	for column_i := 0; column_i < len(lines[0]); column_i += asciiArtCharLength(&lines, column_i) {
 		// presumably lines always consist of exactly 8 lines
 		printRegularChar(&lines, column_i)
 	}
@@ -75,14 +84,15 @@ EX: go run . --reverse=<fileName>`)
 }
 
 func printRegularChar(lines *[]string, column_index int) {
-	bannerChars, err := os.ReadFile("standard.txt")
+	bannerChars, err := os.ReadFile("./banners/standard.txt")
 	if err != nil {
 		fmt.Println("Error reading standard.txt")
+    return
 	}
 
 	bannerLines := piscine.Split(string(bannerChars), "\n")
 	var i int // index of input line
-	charLength := asciiArtCharLength(lines, column_index) + 1
+	charLength := asciiArtCharLength(lines, column_index)
 
 	for bannerLIndex := 0; bannerLIndex < len(bannerLines) / 8; bannerLIndex++ {
 		tempIndex := bannerLIndex * 8
