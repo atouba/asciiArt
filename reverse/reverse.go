80df28a97b7e195837e88b346831d85b78bdd573
atouba <anasstouba5@gmail.com>
2024-09-17 17:56:08 +0300

    pass the lines variable by value instead
    There was no need to pass it by address

--- reverse/reverse.go
+++ reverse/reverse.go
@@ -19,19 +19,19 @@ func index(str string, char rune, start_index int) int {
 	return i
 }
 
-func isVerticallyEqual(lines *[]string, column_index int) bool {
-	lines_n := len(*lines) - 1 // minus 1 so when iterating we don't avoid out of range error
+func isVerticallyEqual(lines []string, column_index int) bool {
+	lines_n := len(lines) - 1 // minus 1 so when iterating we don't avoid out of range error
 
 	for row := 0; row < lines_n; row++ {
-		if (*lines)[row][column_index] != (*lines)[row + 1][column_index] {
+		if lines[row][column_index] != lines[row + 1][column_index] {
 			return false
 		}
 	}
 	return true
 }
 
-func isSpace(lines *[]string, column_index int) bool {
-	char := (*lines)[0][column_index]
+func isSpace(lines []string, column_index int) bool {
+	char := lines[0][column_index]
 
 	if char == ' ' && isVerticallyEqual(lines, column_index) {
 		return true
@@ -39,18 +39,18 @@ func isSpace(lines *[]string, column_index int) bool {
 	return false
 }
 
-func asciiArtCharLength(lines *[]string, start_index int) int {
+func asciiArtCharLength(lines []string, start_index int) int {
 	length := 0
 
   // check first if it's an ascii art space
   x := start_index
   for ; x < start_index + 6 ; x++ {
-    if !(x < len((*lines)[0])) { log.Fatal("Bad Input") }
+    if !(x < len(lines[0])) { log.Fatal("Bad Input") }
     if !isSpace(lines, x) { break }
   }
   if x == start_index + 6 { return 6 }
 
-	for i := start_index; i < len((*lines)[0]); i++ {
+	for i := start_index; i < len(lines[0]); i++ {
 		if !isSpace(lines, i) {
 			length++
 		} else {
@@ -68,13 +68,13 @@ func Reverse(filename string) {
 	}
 	lines := piscine.Split(string(ascii_art), "\n")
   lines = lines[: len(lines)-1]
-	for column_i := 0; column_i < len(lines[0]); column_i += asciiArtCharLength(&lines, column_i) {
-		printRegularChar(&lines, column_i)
+	for column_i := 0; column_i < len(lines[0]); column_i += asciiArtCharLength(lines, column_i) {
+		printRegularChar(lines, column_i)
 	}
 	fmt.Println()
 }
 
-func printRegularChar(lines *[]string, column_index int) {
+func printRegularChar(lines []string, column_index int) {
 	bannerChars, err := os.ReadFile("./banners/standard.txt")
 	if err != nil {
 		fmt.Println("Error reading standard.txt")
@@ -88,7 +88,7 @@ func printRegularChar(lines *[]string, column_index int) {
 	for bannerLIndex := 0; bannerLIndex < len(bannerLines) / 8; bannerLIndex++ {
 		tempIndex := bannerLIndex * 8
 		for i = 0; i < 8; i++ {
-			if bannerLines[tempIndex] != (*lines)[i][column_index: column_index + charLength] {
+			if bannerLines[tempIndex] != lines[i][column_index: column_index + charLength] {
 				break
 			}
 			tempIndex++
