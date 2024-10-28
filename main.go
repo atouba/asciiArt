306791ba13660f33b82f1467798a0f37de63ecf9
Markus Amberla <markus.amberla@gmail.com>
2024-10-28 09:47:27 +0200

    reinstated font switch functionality

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ import (
 )
 
 func main() {
-  if len(os.Args) != 2 {
-    log.Fatal("Usage: go run . <string>")
-  } else {
-    fmt.Print(basic.Basic(os.Args[1], "standard"))
-  }
+	if len(os.Args) != 2 && len(os.Args) != 3 {
+		log.Fatal("Usage: go run . <text> or go run . <text> <style>")
+	} else if len(os.Args) == 3 {
+		if os.Args[2] != "standard" && os.Args[2] != "shadow" && os.Args[2] != "thinkertoy" {
+			log.Fatal("Invalid style name. Use \"standard\", \"shadow\" or \"thinkertoy\"")
+		}
+		fmt.Print(basic.Basic(os.Args[1], os.Args[2]))
+	} else {
+		fmt.Print(basic.Basic(os.Args[1], "standard"))
+	}
 }
